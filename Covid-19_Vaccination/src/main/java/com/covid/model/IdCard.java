7063c05ea7a550fc53536c3cc84a615aea403acf
Dikshant <Dikshant@DESKTOP-7IMCASB>
2023-11-28 22:01:37 +0530

    covid

---
+++ Covid-19_Vaccination/src/main/java/com/covid/model/IdCard.java
@@ -0,0 +1,28 @@
+package com.covid.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Data
+@Entity
+public class IdCard {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idCardId;
+	private String name;
+	private LocalDate dob;
+	private String gender;
+	private String address;
+	private String state;
+	private String city;
+	private String pincode;
+	
+
+	
+
+}
